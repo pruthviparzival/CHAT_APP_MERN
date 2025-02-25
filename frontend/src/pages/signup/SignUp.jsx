3125a4af7a11b37f6690b2c2b2e56086bcd2ece0
pruthviparzival <pruthvirajsharanappa@gmail.com>
2025-02-26 01:15:14 +0530

    onr more path correction

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import GenderCheckbox from "./genderCheckbox";
+import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
 import useSignup from "../../hooks/useSignup";
 
