c6cc954407de9c9aa9a1a769bf2eeebaaa72e8ac
pruthviparzival <pruthvirajsharanappa@gmail.com>
2025-02-26 01:07:51 +0530

    updated path

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import MessageContainer from "../../components/messagecontainer/MessageContainer";
-import Sidebar from "../../components/sidebar/sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
